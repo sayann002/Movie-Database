daf2a7e34f544114e4316d42bbaa33aba7589cca
Sayan Naskar <47991790+sayann002@users.noreply.github.com>
2021-03-04 23:26:57 +0530

    Uploading Files

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Search({ handleInput, search }) {
+  return (
+    <section>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search for a movie..."
+        className="searchbox"
+        onChange={handleInput}
+        onKeyPress={search}
+      />
+    </section>
+  );
+}
+
+export default Search;
